4975d12ae79fd482426c395f9b334c8fe36c6c4d
4st <forest2087@gmail.com>
2016-03-15 10:55:36 -0400

    Update start.sh

--- Build/start.sh
+++ Build/start.sh
@@ -16,7 +16,9 @@ cd /var/dynamodump/dynamodump
 
 /usr/bin/python dynamodump.py -m restore -r local -s itsqa-emp-synthesis-client-b71289951f7f4c7c81874f0e580286d0 --host localhost --port 7777 --accessKey a --secretKey a
 
-/usr/bin/python dynamodump.py -m backup -r us-east-1 -s itsqa-emp-forms-client-b71289951f7f4c7c81874f0e580286d0
+if [ -d "~/.aws" ]; then
+  /usr/bin/python dynamodump.py -m backup -r us-east-1 -s itsqa-emp-forms-client-b71289951f7f4c7c81874f0e580286d0
+fi
 
 /usr/bin/python dynamodump.py -m restore -r local -s itsqa-emp-forms-client-b71289951f7f4c7c81874f0e580286d0 --host localhost --port 7777 --accessKey a --secretKey a
 
