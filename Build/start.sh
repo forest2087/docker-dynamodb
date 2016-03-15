69e091aff40d34081118b476bce2114e924df5ed
4st <forest2087@gmail.com>
2016-03-15 10:49:46 -0400

    Update start.sh

--- Build/start.sh
+++ Build/start.sh
@@ -10,10 +10,15 @@ cd /var/dynamodump/dynamodump
 
 /usr/bin/python dynamodump.py -m restore -r local -s itsqa-emp-director-client-b71289951f7f4c7c81874f0e580286d0 --host localhost --port 7777 --accessKey a --secretKey a
 
-/usr/bin/python dynamodump.py -m restore -r local -s itsqa-emp-forms-client-b71289951f7f4c7c81874f0e580286d0 --host localhost --port 7777 --accessKey a --secretKey a
+#/usr/bin/python dynamodump.py -m restore -r local -s itsqa-emp-forms-client-b71289951f7f4c7c81874f0e580286d0 --host localhost --port 7777 --accessKey a --secretKey a
 
 /usr/bin/python dynamodump.py -m restore -r local -s itsqa-emp-hub-client-b71289951f7f4c7c81874f0e580286d0 --host localhost --port 7777 --accessKey a --secretKey a
 
 /usr/bin/python dynamodump.py -m restore -r local -s itsqa-emp-synthesis-client-b71289951f7f4c7c81874f0e580286d0 --host localhost --port 7777 --accessKey a --secretKey a
 
-while true; do sleep 1000; done
+/usr/bin/python dynamodump.py -m backup -r us-east-1 -s itsqa-emp-forms-client-b71289951f7f4c7c81874f0e580286d0
+
+/usr/bin/python dynamodump.py -m restore -r local -s itsqa-emp-forms-client-b71289951f7f4c7c81874f0e580286d0 --host localhost --port 7777 --accessKey a --secretKey a
+
+
+while true; do sleep 10000000; done
